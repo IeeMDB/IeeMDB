2d8f08a840ecaaf7c0dcac999cb188402531c5fc
yoBoyio <dimi044@hotmail.com>
2021-05-16 19:52:05 +0300

    Updates

---
+++ precache-manifest.0b7e420df7b592bc14d2835d8524807d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146d0ccdf455c502241659f71ed11d94",
+    "url": "/IeeMDB/index.html"
+  },
+  {
+    "revision": "abe34aad8b70a09731c2",
+    "url": "/IeeMDB/static/css/main.730747a3.chunk.css"
+  },
+  {
+    "revision": "c598ea5a60ba52b600f3",
+    "url": "/IeeMDB/static/js/2.0e540853.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/IeeMDB/static/js/2.0e540853.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe34aad8b70a09731c2",
+    "url": "/IeeMDB/static/js/main.84627ced.chunk.js"
+  },
+  {
+    "revision": "97cb8cc4c6fa38010155",
+    "url": "/IeeMDB/static/js/runtime-main.46f6ad5a.js"
+  },
+  {
+    "revision": "1b483f5f3e73468f0b48a40dcf400b13",
+    "url": "/IeeMDB/static/media/Group 3.1b483f5f.svg"
+  },
+  {
+    "revision": "808a6548be5612f1ec0bbeb0c5fc36da",
+    "url": "/IeeMDB/static/media/preview.808a6548.jpg"
+  }
+]);
