dc2fbb38e04fecee3e5ae8136a9d99e2d3a3721d
yoBoyio <dimi044@hotmail.com>
2021-05-16 20:24:48 +0300

    changes5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           <ul>
             <li style={{ display: 'flex' }}>
               <PlayCircleOutlineIcon style={{ fontSize: 30, padding: '0px 8px' }} />
-              <Link to="/"> IeeMDB </Link>
+              <Link to="/IeeMDB"> IeeMDB </Link>
             </li>
           </ul>
           <ul>
